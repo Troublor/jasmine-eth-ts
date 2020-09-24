f232b6ce616ec003f34e67680b204f890c41cb75
Aaron <troublor@live.com>
2020-09-24 18:21:21 +0800

    export MockEthereum

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import SDK from "./SDK";
 import Account from "./Account";
 import TFC from "./TFC";
+import MockEthereum from "./MockEthereum";
+
 export * from "./types"
 
 export default SDK;
-export {SDK, Account, TFC};
+export {SDK, Account, TFC, MockEthereum};
 // module.exports = SDK
